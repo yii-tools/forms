2b5c05b360cabfe6ff2d2e1f478982944a80d4f0
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-01-31 09:33:44 -0300

    Add `Date::class` widget. (#2)

---
+++ tests/Component/Input/Date/ExceptionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yii\Forms\Tests\Component\Input\Date;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Yii\Forms\Component\Input\Date;
+use Yii\Forms\Tests\Support\TestForm;
+
+final class ExceptionTest extends TestCase
+{
+    public function testStep(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The value must be a number.');
+
+        Date::widget([new TestForm(), 'string'])->step('x')->render();
+    }
+
+    public function testValue(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Date::class widget must be a string or null value.');
+
+        Date::widget([new TestForm(), 'array'])->render();
+    }
+}
